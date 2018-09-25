f4e9f659d528cab168f1d7d3f482dfdb62593b5e
Maxime Guillemot <guillemot.maxime@gmail.com>
2018-09-25 12:35:04 +0200

    Header CSS + Navigation items

--- pages/templates/default.php
+++ pages/templates/default.php
@@ -6,7 +6,7 @@
 		<title>T5C Editor</title>
 
 		<link rel="icon" sizes="16x16" href="images/favicon.ico" />
-		<link href="css/styles.css" type="text/css" rel="stylesheet" media="all" />
+		<link href="css/main_styles.css" type="text/css" rel="stylesheet" media="all" />
 	</head>
 
 	<body>
@@ -16,7 +16,30 @@
 
 		<div id="wrapper" class="flex">
 			<nav>
-				
+				<a href="index.php?p=items">
+					<div id="nav_items"></div>
+					<p id="items_desc">Items</p>
+				</a>
+				<a href="index.php?p=creatures">
+					<div id="nav_creatures"></div>
+					<p id="creatures_desc">Creatures</p>
+				</a>
+				<a href="index.php?p=npcs">
+					<div id="nav_npcs"></div>
+					<p id="npcs_desc">NPCs</p>
+				</a>
+				<a href="index.php?p=clans">
+					<div id="nav_clans"></div>
+					<p id="clans_desc">Clans</p>
+				</a>
+				<a href="index.php?p=spells">
+					<div id="nav_spells"></div>
+					<p id="spells_desc">Spells</p>
+				</a>
+				<a href="index.php?p=flags">
+					<div id="nav_flags"></div>
+					<p id="flags_desc">Flags</p>
+				</a>
 			</nav>
 
 			<main>
